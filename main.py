5b64466f747dda31341f03538e0bcdf238c205ac
Tvicer <sn2002-2011@mail.ru>
2024-05-31 03:21:45 +0300

    final commit

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 import speech_recognition as sr
 from pydub import AudioSegment
+from flask import Flask, request
+import os
 
-# Инициализируем распознаватель речи
+app = Flask(__name__)
 r = sr.Recognizer()
 
+@app.route('/api/voiceToText/send', methods=['POST'])
+def index():
+    json = request.get_json()
 
-# Функция для распознавания речи из аудио файла
+    audio_files = ['C:/Users/Tvicer/Downloads/' + json["voice"]]
+
+    for audio_file in audio_files:
+        text, duration = transcribe_audio(audio_file)
+        os.remove('C:/Users/Tvicer/Downloads/' + json["voice"])
+        return text
+
 def transcribe_audio(audio_file):
-    sound = AudioSegment.from_file(audio_file, format="webm")  # Загружаем MP3 файл
-    wav_file = "temp.wav"  # Временный WAV файл
-    sound.export(wav_file, format="wav")  # Конвертируем MP3 в WAV
+    sound = AudioSegment.from_file(audio_file, format="webm")
+    wav_file = "temp.wav"
+    sound.export(wav_file, format="wav")
 
     with sr.AudioFile(wav_file) as source:
-        audio_data = r.record(source)  # читаем аудио файл
-        duration = source.DURATION  # получаем длительность аудио
+        audio_data = r.record(source)
+        duration = source.DURATION
 
     try:
-        text = r.recognize_google(audio_data, language='ru-RU')  # распознаем текст на русском языке
+        text = r.recognize_google(audio_data, language='ru-RU')
         return text, duration
     except sr.UnknownValueError:
         print("Распознаватель речи не смог обработать аудио")
-        return None, None  # Возвращаем None для текста и длительности
+        return None, None
     except sr.RequestError as e:
         print(f"Ошибка сервиса распознавания речи: {e}")
-        return None, None  # Возвращаем None для текста и длительности
-
-# Список аудио файлов для обработки
-audio_files = ['c1.webm']
+        return None, None
 
 
-
-# Перебираем аудио файлы и добавляем данные в словарь
-for audio_file in audio_files:
-    text, duration = transcribe_audio(audio_file)
-    if text is not None:  # Проверяем, что текст не равен None
-        print(f"Распознанный текст: {text}")
-        print(f"Длительность аудио: {duration} секунд")
-
+if __name__ == "__main__":
+    app.run(debug=False)
